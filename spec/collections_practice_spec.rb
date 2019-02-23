85e3c9f09cc163e16392af634879c55222269b0a
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-23 23:52:59 +0000

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4 Bonus
+  describe '#swap_elements_from_to' do
+    it 'swap the first and second elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], 0, 1)).to eq(["ashley", "blake", "scott"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
